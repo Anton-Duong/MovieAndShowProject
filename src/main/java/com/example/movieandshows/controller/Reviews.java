8f6191bf4a74dd2252888c16b05506744487f3ef
Anthony Duong <anthonyduong123@gmail.com>
2025-01-07 12:33:08 -0500

    added folders

---
+++ src/main/java/com/example/movieandshows/controller/Reviews.java
@@ -0,0 +1,4 @@
+package com.example.movieandshows.controller;
+
+public class Reviews {
+}
---
+++ src/main/java/com/example/movieandshows/controller/User.java
@@ -0,0 +1,7 @@
+package com.example.movieandshows.controller;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+}
