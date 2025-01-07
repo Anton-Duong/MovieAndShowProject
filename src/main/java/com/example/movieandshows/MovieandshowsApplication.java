5cdd4a5d361cd4d8cc0f540a8a4815801f1d774f
Anthony Duong <anthonyduong123@gmail.com>
2025-01-07 11:59:21 -0500

    first branch

---
+++ src/main/java/com/example/movieandshows/MovieandshowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieandshows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieandshowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieandshowsApplication.class, args);
+	}
+
+}
